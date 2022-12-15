05b340dfa11e40e35031488ba76715e1b860834e
Jhefferson Albornoz <jheffersonjhgg@gmail.com>
2022-12-14 19:56:33 -0500

    Ajuste de Imagenes

--- src/data/compe-general.data.js
+++ src/data/compe-general.data.js
@@ -3,7 +3,7 @@ export default [
     title: 'Liderazgo',
     description: 'Al ver que varios de mis compañeros se tubieron que ir a mitad del curso, tube que tomar la batuta y dirigir el equipo que quedaba, solo eramos 2 personas y habia mucho que hacer, perdi una exposicion, pero eso no me detubo a continuar con el proyecto',
     level: 'Semi-Senior',
-    img: 'https://i.ibb.co/2dMgFv9/Captura-de-pantalla-2022-12-14-184230.png',
+    img: 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/O4OJBW4HT5ETHLCFM6X2XU5EQQ.jpg',
     links: {
       github: '',
       web: 'https://docs.google.com/document/d/1qKAdAX441PyQQl-hnAyZr7VY_7rjHHLV/edit?usp=share_link&ouid=100265047572603070889&rtpof=true&sd=true'
@@ -13,7 +13,7 @@ export default [
     title: 'Pensamiento Crítico',
     description: 'Busque la manera de que el equipo pueda manejar nuevas ideas para que elaboremos el proyecto en base a nuestras ideas.',
     level: 'Junior',
-    img: 'https://i.ibb.co/pKcgc8Y/311.png',
+    img: 'https://rinconpsicologia.com/wp-content/uploads/2021/07/pensamiento-critico-1024x538.jpg',
     links: {
       github: '',
       web: 'https://docs.google.com/document/d/17TFKQpK6H8OVnrpa9qT-u0q4ZxZYeiG5/edit?usp=share_link&ouid=100265047572603070889&rtpof=true&sd=true'
@@ -23,7 +23,7 @@ export default [
     title: 'Comunición Efectiva',
     description: 'En este proyecto me dedique a precentar idea y comunicarlas a mi equipo, dando como resultado que tengamos el alcance los aplicativos a usar.',
     level: 'Semi-Senior',
-    img: 'https://i.ibb.co/DwFqByV/1212.png',
+    img: 'http://buenasnuevas.fm/wp-content/uploads/2022/03/61ae859b9679e33903c7fd25_Que-es-la-comunicacion-efectiva-simla.png',
     links: {
       github: '',
       web: 'https://drive.google.com/file/d/1el8Kge9NRR1RxckBg_Y_A_KHi3fOLrLQ/view?usp=share_link'
@@ -33,7 +33,7 @@ export default [
     title: 'Equipos Interdisciplinarios',
     description: 'Interactue con mis compañeros para que constituir y mejorar cada vez el proyecto. De igualmenara siempre se resptaban las opiniones.',
     level: 'Junior',
-    img: 'https://i.ibb.co/DwFqByV/1212.png',
+    img: 'https://capitalhumano.emol.com/wp-content/uploads/2017/09/idea-made-in-chile-840x400.jpg',
     links: {
       github: '',
       web: 'https://drive.google.com/file/d/1el8Kge9NRR1RxckBg_Y_A_KHi3fOLrLQ/view?usp=share_link'
@@ -43,7 +43,7 @@ export default [
     title: 'Resolución de Problemas',
     description: 'Cuando ma mentora nos decia que la mayor parte del trabajo esta mal, me encarge de plantear una idea para el equipo y poder resolver mejor el proyecto. La mentora nos felicito diciento que ahora se ve mas organizado.',
     level: 'Senior',
-    img: 'https://i.ibb.co/pKcgc8Y/311.png',
+    img: 'https://web-static.wrike.com/blog/content/uploads/2017/03/tecnicas-consejos-resolucion-problemas.jpg?av=ad62e6dea2ae256b24400f3d2aaf19c5',
     links: {
       github: '',
       web: 'https://docs.google.com/document/d/17TFKQpK6H8OVnrpa9qT-u0q4ZxZYeiG5/edit?usp=share_link&ouid=100265047572603070889&rtpof=true&sd=true'
--- src/data/compe-specific.data.js
+++ src/data/compe-specific.data.js
@@ -1,36 +1,36 @@
 export default [
   {
-    title: 'Liderazgo',
+    title: 'Gestión de Proyectos',
     description: 'Al ver que varios de mis compañeros se tubieron que ir a mitad del curso, tube que tomar la batuta y dirigir el equipo que quedaba, solo eramos 2 personas y habia mucho que hacer, perdi una exposicion, pero eso no me detubo a continuar con el proyecto',
     level: 'Semi-Senior',
-    img: 'https://i.ibb.co/2dMgFv9/Captura-de-pantalla-2022-12-14-184230.png',
+    img: 'https://www.ceupe.eu/images/easyblog_articles/230/ges_proyec.png',
     links: {
       github: '',
       web: 'https://docs.google.com/document/d/1qKAdAX441PyQQl-hnAyZr7VY_7rjHHLV/edit?usp=share_link&ouid=100265047572603070889&rtpof=true&sd=true'
     }
   },
   {
-    title: 'Pensamiento Crítico',
+    title: 'Investigacion y Diagnóstico',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
     level: 'Semi-Senior',
-    img: 'https://i.ibb.co/DwFqByV/1212.png',
+    img: 'https://durviz.com/wp-content/uploads/2016/06/Investigacion-1.jpg',
     links: {
       github: '',
       web: 'https://docs.google.com/document/d/17TFKQpK6H8OVnrpa9qT-u0q4ZxZYeiG5/edit?usp=share_link&ouid=100265047572603070889&rtpof=true&sd=true'
     }
   },
   {
-    title: 'Comunición Efectiva',
+    title: 'Desarrollo de Soluciones Innovadoras',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
     level: 'Semi-Senior',
-    img: 'https://i.ibb.co/HnK7n5Q/3111123.png',
+    img: 'https://tuataras.net/wp-content/uploads/Persona-programando.png',
     links: {
       github: 'https://github.com/jalbornoz14/proyecto_upal_fundamentos.git',
       web: ''
     }
   },
   {
-    title: 'Equipos Interdisciplinarios',
+    title: 'Calidad de Software',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
     level: 'Semi-Senior',
     img: 'https://i.ibb.co/HnK7n5Q/3111123.png',
